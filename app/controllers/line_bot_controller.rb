e98beadd8acad073e15fb3b4cc2eb355aab3addc
kei-600th <kei19941005@gmail.com>
2021-10-21 22:33:31 +0900

    5-8 ~ 5-10

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -66,5 +66,39 @@ class LineBotController < ApplicationController
     end
 
   end
+
+  def set_carousel(hotels)
+    bubbles = []
+    hotels.each do |hotel|
+      bubbles.push set_bubble(hotel[0]['hotelBasicInfo'])
+    end
+    {
+      type: 'carousel',
+      contents: bubbles
+    }
+  end
+
+  def set_bubble(hotel)
+    {
+      type: 'bubble',
+      hero: set_hero(hotel),
+      body: set_body(hotel),
+      footer: set_footer(hotel)
+    }
+  end
+
+  def set_hero(hotel)
+    {
+      type: 'image',
+      url: hotel['hotelImageUrl'],
+      size: 'full',
+      aspectRatio: '20:13',
+      aspectMode: 'cover',
+      action: {
+        type: 'uri',
+        uri:  hotel['hotelInformationUrl']
+      }
+    }
+  end
   
 end
